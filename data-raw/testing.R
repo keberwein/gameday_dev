906a5d6d926ad2f2b3f374e428735e910d1b4a07
Keberwein <kristian.eberwein@yahoo.com>
2018-01-10 11:41:16 -0500

    testing action

---
+++ data-raw/testing.R
@@ -0,0 +1,20 @@
+library(gamedaydev)
+
+innings_df <- get_payload(start = "2016-08-03", end = "2016-08-04")
+
+#Matching event numbers are under pitch and action. Pitch is a child of atbat, maybe join like that?
+# I'm already grabbing num from atbat and inserting it into the pitch table--could probably join pitch and action on event_num and grab num from there.
+# 
+
+# Every runner at an AB "num", there are some double when there was more than one runner--that's OK.
+
+# This creates a one to one match on event_num and AB num.
+z= select(runner, num, event_num, gameday_link)
+
+
+### Looks like event_nums are getting crossed up on the xml piece of the action dataframe. In fact, just about everything in action is wrong.
+### Need to to a long test of a single game.
+
+
+# Need to join the matches to action.
+test <- dplyr::left_join(action, z, by = c("event_num", "gameday_link"))
