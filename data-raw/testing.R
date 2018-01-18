f71a911cc517d1165c5e42adec701c5f377ca2eb
Keberwein <kris.eberwein@stackpath.com>
2018-01-18 09:15:57 -0500

    action testing

--- data-raw/testing.R
+++ data-raw/testing.R
@@ -1,22 +1,22 @@
 library(gamedaydev)
+library(dplyr)
 
 innings_df <- get_payload(start = "2016-08-03", end = "2016-08-04")
 
-action <- innings_df$action # This subset was messing up columns. %>% subset(gameday_link == "/gid_2016_08_04_chamlb_detmlb_1")
-
-
-#Matching event numbers are under pitch and action. Pitch is a child of atbat, maybe join like that?
-# I'm already grabbing num from atbat and inserting it into the pitch table--could probably join pitch and action on event_num and grab num from there.
-# 
+action <- innings_df$action
+runner <- innings_df$runner
 
 # Every runner at an AB "num", there are some double when there was more than one runner--that's OK.
 
-# This creates a one to one match on event_num and AB num.
+
+# Looks like the flow is, action -> atbat, runner.
+# 
+# Matching event_num for runner and actioni won't work. Look at actioin and runner at the top of this xml.
+# http://gd2.mlb.com/components/game/mlb/year_2016/month_08/day_04/gid_2016_08_04_chamlb_detmlb_1/inning/inning_all.xml
+
+
 z= select(runner, num, event_num, gameday_link)
 
-
-### Looks like event_nums are getting crossed up on the xml piece of the action dataframe. In fact, just about everything in action is wrong.
-### Need to to a long test of a single game.
 
 
 # Need to join the matches to action.
