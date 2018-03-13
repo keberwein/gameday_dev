6c5dce481efae407369abc509a30b83e8c57c957
keberwein <kristian.eberwein@yahoo.com>
2018-03-13 10:04:02 -0400

    fixed linscore data order

--- data-raw/testing.R
+++ data-raw/testing.R
@@ -1,51 +1,20 @@
-library(mlbgameday)
-library(dplyr)
+library(doParallel)
 library(DBI)
 library(RSQLite)
-library(doParallel)
-
-
 
 con <- dbConnect(RSQLite::SQLite(), dbname = "gameday.sqlite3")
 
-library(pitchRx)
-
-scrape(start = "2016-08-03", end = "2016-08-03", con = con)
-
-
+# First we need to register our parallel cluster.
+# Set the number of cores to use as the machine's maximum number of cores minus 1 for background processes.
 no_cores <- detectCores() - 2
 cl <- makeCluster(no_cores)  
 registerDoParallel(cl)
 
-get_payload(start = "2016-08-04", end = "2016-08-04", db_con=con)
+#linescore <- get_payload(start = "2017-01-01", end = "2018-01-01", dataset = "linescore")
+
+get_payload(start = "2017-01-01", end = "2018-01-01", dataset = "linescore", db_con = con)
+
 
 stopImplicitCluster()
 rm(cl)
-
-
-
-z <- dbGetQuery(con,"SELECT * FROM po limit 5")
-
-
-
-
-library(pitchRx)
-
-cdatt <- scrape(start = "2016-08-03", end = "2016-08-03")
-
-innings <- get_payload(start = "2016-08-04", end = "2016-08-04")
-
-
-atbat = innings$atbat
-action = innings$action
-pitch = innings$pitch
-runner = innings$runner
-po = innings$po
-
-
-catbat = cdatt$atbat
-caction = cdatt$action
-cpitch = cdatt$pitch
-crunner = cdatt$runner
-cpo = cdatt$po
 
