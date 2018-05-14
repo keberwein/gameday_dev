8c0c425980f00197267976ce8709b1b6e60ce8e2
keberwein <kristian.eberwein@yahoo.com>
2018-05-14 10:15:31 -0400

    testing data.table

--- data-raw/testing.R
+++ data-raw/testing.R
@@ -2,19 +2,23 @@ library(doParallel)
 library(DBI)
 library(RSQLite)
 
-con <- dbConnect(RSQLite::SQLite(), dbname = "gameday.sqlite3")
-
 # First we need to register our parallel cluster.
 # Set the number of cores to use as the machine's maximum number of cores minus 1 for background processes.
 no_cores <- detectCores() - 2
 cl <- makeCluster(no_cores)  
 registerDoParallel(cl)
 
-#linescore <- get_payload(start = "2017-01-01", end = "2018-01-01", dataset = "linescore")
+# Then run the get_payload function as normal.
 
-get_payload(start = "2017-01-01", end = "2018-01-01", dataset = "linescore", db_con = con)
+s = Sys.time()
 
+innings_df <- get_payload(start = "2017-01-01", end = "2018-01-01")
 
+f = s - Sys.time()
+
+# Don't forget to stop the cluster when finished.
 stopImplicitCluster()
 rm(cl)
 
+
+gidslist <- make_gids(start = "2017-08-03", end = "2017-09-04") 
