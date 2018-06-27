27e9d933b0bd66c6549ab14930d9dee597205d9b
keberwein <kristian.eberwein@yahoo.com>
2018-06-27 15:52:48 -0400

    removed todo added notes

---
+++ data-raw/notes.R
@@ -0,0 +1,12 @@
+z = statcast_payload(start = "2016-04-06", end = "2016-04-15", player_id = 592789, player_type='pitcher')
+
+z = statcast_payload(start = "2016-04-06", end = "2016-04-06")
+
+
+
+# game_pk column now added to gidz. This should be used in place of the game_ids arg to get this funciton to work with get_payload.
+gidz <- gamedaydev::game_ids
+
+                          
+                          
+                          
