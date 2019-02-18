ae340fc178f1384d4552fb04ad06646607260ac0
Kris Eberwein <kristian.eberwein@yahoo.com>
2019-02-18 14:50:52 -0500

    statcast testing

--- R/statcast_dev.R
+++ R/statcast_dev.R
@@ -1,114 +1,56 @@
-# TODO: Re-write params and examples
-# Prot code.
 
-#' This is DEV and needs to be transformed into a payload_method.
-#' 
-#' @param start_date Date of first game for which you want data. Format must be in YYYY-MM-DD format.
-#' @param end_date Date of last game for which you want data. Format must be in YYYY-MM-DD format.
-#' @param playerid The MLBAM ID for the player who's data you want to query.
-#' @param player_type The player type. Can be 'batter' or 'pitcher'
-#' @importFrom readr read_csv
-#' @export
-#' @examples
-#' \dontrun{
-#' scrape_statcast_savant(start_date = "2016-04-06", end_date = "2016-04-15", playerid = 621043, player_type='batter')
-#'
-#' scrape_statcast_savant(start_date = "2016-04-06", end_date = "2016-04-15", playerid = 592789, player_type='pitcher')
-#'
-#' scrape_statcast_savant(start_date = "2016-04-06", end_date = "2016-04-06")
-#' }
 
-scrape_statcast_savant <- function(start_date, end_date, playerid=NULL, player_type=NULL) {
-    # Check to make sure args are in the correct format.
-    if(is.null(player_type)) player_type <- "batter"
-    if(!is.character(start_date) | !is.character(end_date)) message("Please wrap your dates in quotations in 'yyyy-mm-dd' format.")
-    if(as.Date(start_date)<="2015-03-01") message("Some metrics such as Exit Velocity and Batted Ball Events have only been compiled since 2015.")
-    if(as.Date(start_date)<="2008-03-25") message("The data are limited to the 2008 MLB season and after.")
-    if(as.Date(start_date)==Sys.Date()) message("The data are collected daily at 3 a.m. Some of today's games may not be included.")
-    if(as.Date(start_date)>as.Date(end_date)) message("The start date is later than the end date.")
-    #if(is.null(plyaer_type)) plyaer_type <- ""
-    #if(is.null(playerid)) plyaer_type <- ""
-    
-    # extract season from start_date
-    
-    year <- substr(start_date, 1,4)
-    
-    # Looks like the url parsing could be much cleaner.
-    # The else if statements are obnoxious, could probably replace with a single case statement.
-    
-    base_url <- "https://baseballsavant.mlb.com/statcast_search/csv?all=true&hfPT=&hfAB=&hfBBT=&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R%7C&hfC=&hfSea="
+library(jsonlite)
+library(dplyr)
+library(purrr)
+# url with some information about project in Andalussia
+url <- 'http://statsapi.mlb.com/api/v1/game/529572/playByPlay.json'
 
-    # Test case 4 is fixed. Still need to do individual pitcher and batter, and also all pitchers and all batters.
-    
-    if(is.null(playerid) & is.null(player_type)) {
-        message("No player_type specified. Player_type will default to 'batter'.")
-        message("No playerid specified. Collecting data for all batters.")
-        url <- paste0(base_url, 
-                      year, "%7C&hfSit=&player_type=batter&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt=", 
-                      start_date, "&game_date_lt=", 
-                      end_date, "&team=&position=&hfRO=&home_road=&hfFlag=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=pitches&player_event_sort=h_launch_speed&sort_order=desc&min_abs=0&type=details&")
-        
-    } else if (!is.null(playerid) & is.null(player_type)) {
-        message("No player_type specified. Player_type will default to 'batter'.")
-        url <- paste0(base_url, 
-                      year, "%7C&hfSit=&player_type=batter&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt=", 
-                      start_date, "&game_date_lt=", 
-                      end_date, "&team=&position=&hfRO=&home_road=&batters_lookup%5B%5D=", 
-                      playerid, "&hfFlag=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=pitches&player_event_sort=h_launch_speed&sort_order=desc&min_abs=0&type=details&")
-        
-    } else if (!is.null(playerid) & player_type=='batter') {
-        url <- paste0(base_url, 
-                      year, "%7C&hfSit=&player_type=", 
-                      player_type, "&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt=", 
-                      start_date, "&game_date_lt=", 
-                      end_date, "&team=&position=&hfRO=&home_road=&batters_lookup%5B%5D=", 
-                      playerid, "&hfFlag=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=pitches&player_event_sort=h_launch_speed&sort_order=desc&min_abs=0&type=details&")
-        
-    } else if (!is.null(playerid) & player_type=='pitcher') {
-        url <- paste0(base_url, 
-                      year, "%7C&hfSit=&player_type=", 
-                      player_type, "&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt=", 
-                      start_date, "&game_date_lt=", 
-                      end_date, "&team=&position=&hfRO=&home_road=&pitchers_lookup%5B%5D=", 
-                      playerid, "&hfFlag=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=pitches&player_event_sort=h_launch_speed&sort_order=desc&min_abs=0&type=details&")
-        
-    } else if (is.null(playerid) & player_type=='pitcher'){
-        message("Collecting data for all pitchers for dates specified.")
-        url <- paste0(base_url, 
-                      year, "%7C&hfSit=&player_type=pitcher&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt=", 
-                      start_date, "&game_date_lt=", 
-                      end_date, "&team=&position=&hfRO=&home_road=&&hfFlag=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=pitches&player_event_sort=h_launch_speed&sort_order=desc&min_abs=0&type=details&")
-        
-    } else {
-        message("Collecting data for all batters for dates specified.")
-        url <- paste0(base_url, 
-                      year, "%7C&hfSit=&player_type=batter&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt=", 
-                      start_date, "&game_date_lt=", 
-                      end_date, "&team=&position=&hfRO=&home_road=&&hfFlag=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=pitches&player_event_sort=h_launch_speed&sort_order=desc&min_abs=0&type=details&")
-    }
-    
+cont = jsonlite::fromJSON(url, flatten = T)
+allplays = cont$allPlays
 
-    
-    tryCatch(
-        {
-            print("These data are from BaseballSevant and are property of MLB Advanced Media, L.P. All rights reserved.")
-            print("Grabbing data, this may take a minute...")
-            payload <- read_csv(url)
-            processed_payload <- process_statcast_payload(payload)
-            message("URL read and payload acquired successfully.")
-            return(processed_payload)
-        },
-        error=function(cond) {
-            message(paste("URL does not seem to exist, please check your Internet connection:"))
-            message("Original error message:")
-            message(cond)
-            return(NA)
-        },
-        message=function(cond) {
-            message(paste("URL caused a message. Make sure your playerid, player_type, and date range are correct:"))
-            message("Original message message:")
-            message(cond)
-            return(NULL)
-        }
-    )
+playevents <- purrr::map_dfr(seq_along(allplays$playEvents), function(x) {
+    out <- data.frame(allplays$playEvents[[x]])
+    #out$inning <- allplays$about.inning
+    out
+})
+
+
+
+for(p in seq_along(allplays)){
+    #print(cont$allPlays$result.event)
+    playevents <- purrr::map_dfr(seq_along(allplays$playEvents), function(x) {
+        out <- data.frame(allplays$playEvents[[x]])
+        out$result.type <- allplays$result.type[[x]]
+        out$result.event <- allplays$result.event[[x]]
+        out$result.typeType <- allplays$result.eventType[[x]]
+        out$result.description <- allplays$result.description[[x]][[1]]
+        out$result.rbi <- allplays$result.rbi[[x]]
+        out$result.awayScore <- allplays$result.awayScore[[x]]
+        out$result.homeScore <- allplays$result.homeScore[[x]]
+        out$about.atBatIndex <- allplays$about.atBatIndex[[x]]
+        out$about.halfInning <- allplays$about.halfInning[[x]]
+        out$about.inning <- allplays$about.inning[[x]]
+        out$about.startTime <- allplays$about.startTime[[x]]
+        out$about.endTime <- allplays$about.endTime[[x]]
+        out$about.isComplete <- allplays$about.isComplete[[x]]
+        out$about.isScoringPlay <- allplays$about.isScoringPlay[[x]]
+        out$about.hasReview <- allplays$about.hasReview[[x]]
+        out$about.hasOut <- allplays$about.hasOut[[x]]
+        out$about.captivatingIndex <- allplays$about.captivatingIndex[[x]]
+        out$matchup.batterId <- allplays$matchup.batter.id[[x]]
+        out$matchup.batterFullName <- allplays$matchup.batter.fullName[[x]]
+        out$matchup.batSideCode <- allplays$matchup.batSide.code[[x]]
+        out$matchup.batSideDes <- allplays$matchup.batSide.description[[x]]
+        out$matchup.pitcherId <- allplays$matchup.pitcher.id[[x]]
+        out$matchup.pitcherFullName <- allplays$matchup.pitcher.fullName[[x]]
+        out$matchup.pitchHandCode <- allplays$matchup.pitchHand.code[[x]]
+        out$matchup.pitchHandDes <- allplays$matchup.pitchHand.description[[x]]
+        out
+    }) 
 }
+
+
+z = filter(playevents, !is.na(hitData.location))
+
+
